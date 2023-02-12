ef29f06fce61536a0b6e29c7b553ca81ae34c47a
anmol dhiman <anmoldhiman7111@gmail.com>
2023-02-13 01:11:07 +0530

    other updates

--- Learning Android App/Kotlin_Java_Mix/app/src/main/java/com/example/kotlin_java_mix/Helper.java
+++ Learning Android App/Kotlin_Java_Mix/app/src/main/java/com/example/kotlin_java_mix/Helper.java
@@ -1,2 +1,9 @@
-package com.example.kotlin_java_mix;public class Helper {
+package com.example.kotlin_java_mix;
+
+import android.widget.TextView;
+
+public class Helper {
+    public void updateFromJava(TextView view){
+        view.setText("hello from java");
+    }
 }
--- Learning Android App/Kotlin_Java_Mix/app/src/main/java/com/example/kotlin_java_mix/MainActivity.kt
+++ Learning Android App/Kotlin_Java_Mix/app/src/main/java/com/example/kotlin_java_mix/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.kotlin_java_mix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.kotlin_java_mix.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.kotlin.setOnClickListener {
+            binding.textView.text = "hello from kotlin"
+        }
+        binding.java.setOnClickListener {
+            val javaHelper = Helper()
+            javaHelper.updateFromJava(binding.textView)
+        }
+
     }
 }
